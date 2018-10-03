bb0cbb14edf1a3139490a7c54643c38e37f9b400
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-03 23:39:29 +0300

    JavaScript. Veriables.

---
+++ JavaScript/Common.js
@@ -0,0 +1,12 @@
+var str="Wep-developer",
+			number = 1,
+			numberSecond = 2.4,
+			bool = true,
+			undef,
+			nuull = null,
+			obj = {
+				prop: "yo";
+				prop: function () {
+				}
+			};
+		alert (obj);
