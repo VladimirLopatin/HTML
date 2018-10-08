17d0e63ea0ab8854b011d9e1dec017bffa0917af
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-08 19:42:38 +0300

    Lesson4, continuation of the lesson3. Условные и логические операторы.

---
+++ JavaScript/Common4.js
@@ -0,0 +1,41 @@
+//var statement; // инструкция
+//statement = 5+2; // выражение
+//"hjjcs"; // литералы
+
+//var str = "lkjfsl";
+
+// Операторы
+
+//Арифметические
+//3+2 //5
+//Условные
+//var age = 51;
+
+//if (age === 18) {
+//	alert ("You are not boy!");
+//}
+//else if (age === 21) {
+//	alert ("Take your beer");
+//}
+//else if (age === 24) {
+//	alert ("You are man");
+//}
+//else {
+//	alert ("You are boy!");
+//}
+
+//(age === 18) ? alert ("You are not boy!") :
+//			   (age === 21) ? alert ("Take your beer") :
+//			   (age === 24) ? alert ("You are man") :
+//			   				  alert ("You are boy!")
+
+//Логические
+//|| // или
+//&& // и
+//! // не
+
+//(age >= 18 && age <= 50) ? alert ("You are man") : alert ("You are boy or old man")
+
+var transport = "мопед";
+(transport !== "ноги" || transport === "велосипед" ) ? alert ("Вы доехали до дома быстро") : alert ("Пешком идти далековато")
+//Побитовые
