68e2582e0bf7ba82fd8b237889cef972de3af4e5
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-10 23:26:33 +0300

    Lesson6. Data type, string. Methods.

---
+++ JavaScript/Common6.js
@@ -0,0 +1,29 @@
+var str = '"string" is a data type';
+	str2 = "\"string\" is a data type";
+	longStr = 'длинная\n \tстрока какая-то\n получилась';
+
+	//console.log ( longStr );
+
+	//console.log ( str2.length );
+	//console.log ( 'hello' + 'world');
+	//console.log ( 'hello ' + 'world');
+
+	//onsole.log ( 'hello ' .concat('world') );
+
+	//console.log ( str.charAt(0) );
+	//console.log ( str.charCodeAt(0) );
+
+	//console.log ( str.substring(5) );
+	//console.log ( str.substring(0,8) );
+	//console.log ( str.slice(-5) );
+	//console.log ( str.substr(14,4) );
+
+	//console.log ( str.split(' ') );
+
+	//console.log ( str.replace('string', 'number') );
+
+	//console.log ( str.indexOf('a'));
+
+	console.log ( str.toUpperCase());
+
+	console.log ( str.toLowerCase());
