f7b3f02f7c2cf17abcb52c1cc75faa5a037e4da1
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-11 18:46:28 +0300

    Lesson8. Type conversion methods.

---
+++ JavaScript/Common8.js
@@ -0,0 +1,20 @@
+
+//console.log(typeof String(455));
+//console.log(typeof Number('455'));
+//console.log(typeof Boolean(455));
+
+//console.log ( typeof (5 + '1') );
+
+//console.log ( typeof (46574 + '') ); //быстрое преобразование в строку
+//console.log ( typeof (+'46574') ); //быстрое преобразование в число
+//console.log ( +'' );
+//console.log ( +true );
+//console.log ( +false );
+//console.log ( typeof (!!'46574') ); //быстрое преобразование в Boolean
+//console.log ( !!'46574' );
+
+var num = 777;
+console.log ( typeof num.toString() );
+
+console.log ( parseInt( '100.24 px, 10' ) );
+console.log ( parseFloat( '100.24 px' ) );
