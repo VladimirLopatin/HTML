ca6760c987b0d4a8698c75f31fec32264f7285d4
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-11 13:08:17 +0300

    Lesson7. Boolean.

---
+++ JavaScript/Common7.js
@@ -0,0 +1,38 @@
+//var booll = false;
+
+//if (!booll) {
+//	console.log ('Hello guys!');
+//}
+
+var obj = {},
+	param,
+	arr = [1,2,3];
+
+//console.log ( Boolean(NaN) );
+//console.log ( Boolean('') );
+//console.log ( Boolean(0) );
+//console.log ( Boolean(undefined) );
+//console.log ( Boolean(null) );
+
+//console.log ( Boolean('String'));
+//console.log ( Boolean(45376) );
+//console.log ( Boolean(obj));
+
+var booll = 'str';
+
+if (booll) {
+//	console.log ('Hello guys!');
+}
+
+
+console.log ( null === undefined);
+
+var func = function(arg) {
+console.log (arg);
+};
+
+console.log ( param );
+console.log ( obj.parampam );
+console.log ( arr [2] );
+
+func('Fox');
