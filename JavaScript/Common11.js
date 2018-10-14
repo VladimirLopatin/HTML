5aa7f670900b26d4f1c9efc40484605f25706ea6
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-14 23:12:51 +0300

    Lesson11. Cycles.

---
+++ JavaScript/Common11.js
@@ -0,0 +1,40 @@
+var i;
+
+//цикл for
+var arr = [1,2,3],
+	len = arr.length;
+//for ( i = 0; i < len; i++) {
+//	console.log ( arr[i] *2 );
+//}
+
+//for ( i = 10; i > 0; i--) {
+//	console.log ( i );
+//
+//цикл for in
+
+//var obj = {
+//	propp : 'one',
+//	proppSecond : 'two'
+//},
+//	prop;
+
+//for ( prop in obj ) {
+//	if ( obj.hasOwnProperty(prop)) {
+//	console.log ( prop + ' : ' + obj[prop] );	
+//	}
+
+//}
+
+// цикл while
+
+//var k = 0;
+//while (k<10) {
+//	console.log ( k++ );
+//}
+
+// цикл do while
+
+var h = 0;
+do {
+	console.log ( h ++ );
+}	while (h<10);
