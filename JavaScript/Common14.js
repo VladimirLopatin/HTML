13fd1691c882dc41b56fefaea2ceec56caa9918f
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-10-18 22:54:28 +0300

    Lesson14. Closure in JS.

---
+++ JavaScript/Common14.js
@@ -0,0 +1,17 @@
+
+var firstFunc = function () {
+	var index = 5;
+
+	return function () {
+		return index;
+	}
+};
+
+
+var secondFunc = function () {
+	var index = 15;
+
+	console.log ( firstFunc()() );
+};
+
+secondFunc();
